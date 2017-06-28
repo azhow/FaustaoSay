0a27691690ed74bdb75271da197159470ea3b433
Lain <arthur.zachow@gmail.com>
2017-06-28 20:51:05 -0300

    delay audio

--- generator.py
+++ generator.py
@@ -5,13 +5,14 @@ import argparse
 import os.path
 import earley
 import wave
+import time
 
 hasAudio = True
 try:
     import pyaudio
 except ImportError:
     hasAudio = False
-    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sentence generator: \
                                                   generates a sentence from the language \
@@ -22,15 +23,13 @@ if __name__ == '__main__':
                                                     .wav files named accordingly \
                                                     with the terminal they represent')
     args = parser.parse_args()
-    
-    
-    
+
     if os.path.isfile(args.grammar):
         initial, variables, terminals, rules = earley.read_file(args.grammar)
     else:
         print(parser.prog + ': error: file ' + args.grammar + ' not found.')
         sys.exit(-1)
-        
+
     playAudio = False
     if args.f:
         if os.path.isdir(args.f):
@@ -38,15 +37,12 @@ if __name__ == '__main__':
         else:
             print(parser.prog + ': error: directory ' + args.f + ' not found.')
             sys.exit(-2)
-            
-    
-    
+
     frase = earley.generate_random(initial, variables, terminals, rules, args.verbose)
     if args.verbose:
         print('\nFrase gerada: ', end='')
     print(frase)
-    
-    
+
     if playAudio and hasAudio:
         audioDir = os.path.abspath(args.f)
         p = pyaudio.PyAudio()
@@ -58,17 +54,19 @@ if __name__ == '__main__':
             else:
                 print(parser.prog + ': error: missing audio file for terminal ' + word + '.')
                 sys.exit(-3)
-                
+
         for audioFile in audioFiles:
             chunk = 1024
-            stream = p.open(format=p.get_format_from_width(audioFile.getsampwidth()),  
-                            channels=audioFile.getnchannels(),  
-                            rate=audioFile.getframerate(),  
+            stream = p.open(format=p.get_format_from_width(audioFile.getsampwidth()),
+                            channels=audioFile.getnchannels(),
+                            rate=audioFile.getframerate(),
                             output=True)
             data = audioFile.readframes(chunk)
             while data:
-                stream.write(data)  
+                stream.write(data)
                 data = audioFile.readframes(chunk)
-            stream.stop_stream()  
+            stream.stop_stream()
             stream.close()
-        p.terminate() 
+            time.sleep(0.1)
+
+        p.terminate()
